54ea0c634f6908684d1af880bfcf61d23e85b476
渡辺孝之 <na8esin.mpst@gmail.com>
2021-10-07 21:16:18 +0900

    GoogleSignInが成功してデバッグでユーザ情報が見れたところ

---
+++ lib/src/auth/auth.dart
@@ -0,0 +1,49 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+Future<UserCredential?> signInWithGoogle() async {
+  // Trigger the authentication flow
+  final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+  if (googleUser == null) {
+    return null;
+  }
+
+  // Obtain the auth details from the request
+  final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+
+  // Create a new credential
+  final credential = GoogleAuthProvider.credential(
+    accessToken: googleAuth.accessToken,
+    idToken: googleAuth.idToken,
+  );
+
+  // Once signed in, return the UserCredential
+  return await FirebaseAuth.instance.signInWithCredential(credential);
+}
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MaterialApp(
+    home: Scaffold(
+      body: MyBody(),
+    ),
+  ));
+}
+
+class MyBody extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ElevatedButton(
+        child: Text('SIGN IN'),
+        onPressed: () async {
+          final user = await signInWithGoogle();
+          print(user);
+        },
+      ),
+    );
+  }
+}
