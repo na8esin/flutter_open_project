957f2a089e2c14b57f74b111964871b2b9e4786f
渡辺孝之 <na8esin.mpst@gmail.com>
2021-10-09 13:33:20 +0900

    auth: 単純にifでwidgetを切り替えてる場合は、signOutするだけでwidgetが切り替わる

--- lib/src/auth/auth.dart
+++ lib/src/auth/auth.dart
@@ -2,6 +2,13 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+// authの仕組みを理解するサンp
+
+final authStateChangesProvider = StreamProvider((ref) {
+  return FirebaseAuth.instance.authStateChanges();
+});
 
 Future<UserCredential?> signInWithGoogle() async {
   // Trigger the authentication flow
@@ -26,23 +33,63 @@ Future<UserCredential?> signInWithGoogle() async {
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(MaterialApp(
-    home: Scaffold(
-      body: MyBody(),
-    ),
-  ));
+  runApp(ProviderScope(child: MyApp()));
 }
 
-class MyBody extends StatelessWidget {
+class MyApp extends HookConsumerWidget {
+  @override
+  Widget build(BuildContext context, ref) {
+    return MaterialApp(
+        home: Scaffold(
+      body: AuthWidget(),
+    ));
+  }
+}
+
+class AuthWidget extends HookConsumerWidget {
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return ref.watch(authStateChangesProvider).when(
+        data: (user) {
+          if (user == null) return SignInScreen();
+          return WellcomeScreen();
+        },
+        error: (e, s, d) => Center(child: Text('$e')),
+        loading: (d) => CircularProgressIndicator());
+  }
+}
+
+class SignInScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
       child: ElevatedButton(
         child: Text('SIGN IN'),
         onPressed: () async {
-          final user = await signInWithGoogle();
-          print(user);
+          // googleアカウントを選択するポップアップが起動して選択するとログインできる
+          await signInWithGoogle();
         },
+      ),
+    );
+  }
+}
+
+class WellcomeScreen extends StatelessWidget {
+  const WellcomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text('Wellcome'),
+          ElevatedButton(
+              onPressed: () async {
+                await FirebaseAuth.instance.signOut();
+              },
+              child: Text('SIGN OUT'))
+        ],
       ),
     );
   }
