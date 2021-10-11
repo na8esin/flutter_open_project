35072e77765f967537f69b690dd554dea7523fe9
渡辺孝之 <na8esin.mpst@gmail.com>
2021-10-11 13:22:39 +0900

    Projectモデル作成

---
+++ lib/src/projects/models/project.dart
@@ -0,0 +1,15 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:flutter/foundation.dart';
+
+part 'project.freezed.dart';
+part 'project.g.dart';
+
+@freezed
+class Project with _$Project {
+  factory Project({
+    required String id,
+    String? title,
+  }) = _Project;
+  factory Project.fromJson(Map<String, Object?> json) =>
+      _$ProjectFromJson(json);
+}
---
+++ lib/src/projects/models/project.g.dart
@@ -0,0 +1,18 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'project.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$_Project _$$_ProjectFromJson(Map<String, dynamic> json) => _$_Project(
+      id: json['id'] as String,
+      title: json['title'] as String?,
+    );
+
+Map<String, dynamic> _$$_ProjectToJson(_$_Project instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'title': instance.title,
+    };
