88a94e5571721fbef2dc9d61f5a9684830491440
渡辺孝之 <na8esin.mpst@gmail.com>
2021-10-11 16:20:03 +0900

    ProjectsScreenを分離

---
+++ lib/src/projects/projects_screen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+import 'project_provider.dart';
+
+class ProjectsScreen extends HookConsumerWidget {
+  const ProjectsScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, ref) {
+    final asyncValue = ref.watch(projectsProvider);
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(AppLocalizations.of(context)!.projectsTitle),
+          ListView(
+            children: [
+              ...asyncValue.when(
+                  data: (data) {
+                    return data.docs
+                        .map((e) => ListTile(
+                              title: Text('${e.data().title}'),
+                            ))
+                        .toList();
+                  },
+                  error: (e, s, _) => [Text(e.toString())],
+                  loading: (_) => [CircularProgressIndicator()])
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
