fcd846bbe947b1d3d7511215b4c0735519ab31f2
渡辺孝之 <na8esin.mpst@gmail.com>
2021-10-14 19:59:50 +0900

    プロフィール画面にアバターを表示

--- lib/src/profile/profile_screen.dart
+++ lib/src/profile/profile_screen.dart
@@ -16,75 +16,87 @@ class ProfileScreen extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final asyncValue = ref.watch(profileProvider);
-    // ここのentityはgithubで見るとわかりずらい
-    final entity = asyncValue.when(
-        data: (e) => e!.data(), loading: (_) => null, error: (e, s, _) => null);
-    final nicknameController = useTextEditingController(text: entity?.nickname);
-    final photoUrlController = useTextEditingController(text: entity?.photoUrl);
-    return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.close),
-        onPressed: () {
-          Navigator.of(context).pop();
-        },
-      ),
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              children: [
-                TextFormField(
-                  controller: nicknameController,
-                  decoration: const InputDecoration(
-                    label: Text('nickname'),
-                  ),
-                ),
-                // ここの横にアバターで表示したい
-                TextFormField(
-                  controller: photoUrlController,
-                  decoration: const InputDecoration(
-                    label: Text('photoUrl'),
-                  ),
-                ),
-                ElevatedButton(
-                    onPressed: () {
-                      final user = ref.read(authRepositoryProvider).getUser();
-                      if (user == null) {
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          const SnackBar(
-                              content:
-                                  Text('You are not logged in. Please login.')),
-                        );
-                        return;
-                      }
-                      photoUrlController.text = user.photoURL ?? "";
-                    },
-                    child: Text('Apply Goole Account photoUrl')),
-                ElevatedButton(
-                    onPressed: () {
-                      final user = ref.read(authRepositoryProvider).getUser();
-                      if (user == null) {
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          const SnackBar(
-                              content:
-                                  Text('You are not logged in. Please login.')),
-                        );
-                        return;
-                      }
-                      ref.read(profileRepositoryProvider).set(
-                          user.uid,
-                          Profile(
-                              nickname: nicknameController.text,
-                              photoUrl: photoUrlController.text));
-                    },
-                    child: Text('update'))
-              ],
+    return asyncValue.when(
+        data: (e) {
+          final profile = e!.data();
+          final nicknameController =
+              useTextEditingController(text: profile?.nickname);
+          final photoUrlController =
+              useTextEditingController(text: profile?.photoUrl);
+
+          return Scaffold(
+            floatingActionButton: FloatingActionButton(
+              child: Icon(Icons.close),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
             ),
-          ),
-        ),
-      ),
-    );
+            body: SafeArea(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    children: [
+                      CircleAvatar(
+                        backgroundImage: NetworkImage(photoUrlController.text),
+                      ),
+                      TextFormField(
+                        controller: nicknameController,
+                        decoration: const InputDecoration(
+                          label: Text('nickname'),
+                        ),
+                      ),
+                      TextFormField(
+                        controller: photoUrlController,
+                        decoration: const InputDecoration(
+                          label: Text('photoUrl'),
+                        ),
+                      ),
+                      ElevatedButton(
+                          onPressed: () {
+                            final user =
+                                ref.read(authRepositoryProvider).getUser();
+                            if (user == null) {
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                const SnackBar(
+                                    content: Text(
+                                        'You are not logged in. Please login.')),
+                              );
+                              return;
+                            }
+                            photoUrlController.text = user.photoURL ?? "";
+                          },
+                          child: Text('Apply Goole Account photoUrl')),
+                      ElevatedButton(
+                          onPressed: () {
+                            final user =
+                                ref.read(authRepositoryProvider).getUser();
+                            if (user == null) {
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                const SnackBar(
+                                    content: Text(
+                                        'You are not logged in. Please login.')),
+                              );
+                              return;
+                            }
+                            ref.read(profileRepositoryProvider).set(
+                                user.uid,
+                                Profile(
+                                    nickname: nicknameController.text,
+                                    photoUrl: photoUrlController.text));
+                          },
+                          child: Text('update'))
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          );
+        },
+        loading: (_) => CircularProgressIndicator(),
+        error: (e, s, _) => Center(
+              child: Text(e.toString()),
+            ));
   }
 }
